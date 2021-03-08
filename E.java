bcb3755de2880588a8b3a621581b706b61a359bf
Bhavyu <47683080+Bhavyu@users.noreply.github.com>
2021-03-08 19:53:44 +0530

    Add files via upload

---
+++ E.java
@@ -0,0 +1,21 @@
+class E
+{
+	public static void main(String args[])
+	{
+		for(int i=0;i<7;i++)
+		{
+			for(int j =0;j<7;j++)
+			{
+				if(i%3 ==0 || j==0)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
